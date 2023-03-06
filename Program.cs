269fc8d6778dd7f87c42e507a5b4ee1db802b4a4
Moises Santos <moisessantosddt@gmail.com>
2023-03-06 18:13:03 -0300

    Desafio DIO Smart

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using smartphone.modelos;
+
+
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero:"2142141242", modelo:"235325",imei:"",memoria:64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+
+
+Console.WriteLine("Smartphone Nokia");
+Smartphone iphone = new Iphone(numero:"1231234124", modelo:"32535",imei:"",memoria:128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Whatsapp");
